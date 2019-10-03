5ca336430c8ab653eac05e764427103f708ffc49
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:23:16 -0700

    tomato wip

--- src/extra/Bagers.java
+++ src/extra/Bagers.java
@@ -6,35 +6,41 @@ public class Bagers {
 		String m = "mushroom";
 		String a = "a";
 		String s = "snake";
-		for (int i = 0; i < 16; i++) {
-			if (i < 4 || i == 7 || i == 8 || i == 9 || i == 12 || i== 13 || i== 14) {
+		String saa ="snaaake";
+		String ohitsass="Oooh, its's a snake";
+		for (int i = 0; i < 25; i++) {
+			if (i < 4 || i == 7 || i == 8 || i == 9 || i == 12 || i == 13 || i == 14 || i == 15 || i == 16) {
 				System.out.println(b.toUpperCase() + "," + b + "," + b);
 			}
 			if (i == 4 || i == 10) {
 				System.out.println(m.toUpperCase() + "," + m);
 
 			}
-			if (i == 5) {
+			if (i == 5 || i == 11 || i == 17) {
 				System.out.println();
 			}
 			if (i == 6) {
 				System.out.println(a.toUpperCase() + b + "," + b);
+			
+			}
+			if (i == 12) {
+				System.out.println(a.toUpperCase() + b + "," + b);
 
-				if (i == 12) {
-					System.out.println(a.toUpperCase() + b + "," + b);
-
-				}
-
-				if (i == 13) {
-                    System.out.println(a.toUpperCase()+ b + "," + b  );
-                    
-                if (i == 14) {
-                	System.out.println(a.toUpperCase()+ b + "," + b );
-                }
-				}
-
+			}
+			//17A snake, a snake
+			//18Snaaake! A snaaaake
+			//19Oooh, it's a snake
+			if (i == 17) {
+				System.out.println(a.toUpperCase() + s + "," + s);
+			}
+			if (i == 18) {
+				System.out.println(a.toUpperCase() + saa + "," + saa);
+			}
+			if (i == 19) {
+				System.out.println(a.toUpperCase() + ohitsass+ "," + ohitsass);
 			}
 
 		}
 	}
+
 }
